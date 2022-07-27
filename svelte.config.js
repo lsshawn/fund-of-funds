6c44f8a1ee41375adf5bd58136fb2eb84e55eb3c
ss <coffee.or.tea88@gmail.com>
2022-07-27 23:47:50 +0800

    browser.router false

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-auto'
+// import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,10 +12,9 @@ const config = {
 	],
 	kit: {
 		adapter: adapter(),
-		prerender: {
-			default: false
-		},
-		ssr: false
+		browser: {
+			router: false
+		}
 	}
 }
 
